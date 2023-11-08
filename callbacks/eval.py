7bb445f754e391a5475ec599e71b25eb520cf4b8
Hokyung (Andy) Lee <andy.lee@plpsd.net>
2023-11-08 12:56:17 -0500

    added option to view collected reward

--- callbacks/eval.py
+++ callbacks/eval.py
@@ -9,18 +9,24 @@ def eval(env_name, agent):
     agent.act_net.eval()
     
     total_num_steps = 0
+    total_collected_reward = 0
     num_episodes = 10
     for i in range(num_episodes):
         state = env.reset()
         done = False
         num_steps = 0
+        collected_reward = 0
         while not done:
             # Assuming select_action method does not perform any learning updates
             action = agent.select_action(state, num_action=num_action, exploration=False)  # Disable exploration if necessary
             state, reward, done, _ = env.step(action)
             num_steps += 1
+            collected_reward += reward
         total_num_steps += num_steps
+        total_collected_reward += collected_reward
     avg_num_steps = total_num_steps / num_episodes
+    total_collected_reward = total_collected_reward / num_episodes
     print(f'Average number of steps per episode: {avg_num_steps}')
+    print(f'Average collected reward per episode: {total_collected_reward}')
 
     env.close()
